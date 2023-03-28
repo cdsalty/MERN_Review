6be459bc164dfb136a2aa155f010d8dad4ae854b
cdsalty <soltiscd@gmail.com>
2023-03-28 13:50:02 -0400

    added async handler; rdy to setup database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler');
+
 // @description: Get goals
 // @route: GET /api/goals
 // @access: Public
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'GET goals' });
-};
+});
 
 // @description: Set goals
 // @route: POST /api/goals
 // @access: Public
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   // console.log(req.body);
   if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field for your goal');
   }
   res.status(200).json({ message: 'SET goals' });
-};
+});
 
 // @description: Update all goals
 // @route: PUT /api/goals/:id
 // @access: Private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
   res.json({ message: `Update Goal ${req.params.id}` });
-};
+});
 
 // @description: Get all goals
 // @route: DELETE /api/goals/:id
 // @access: Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
   res.json({ message: `Delete Goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
